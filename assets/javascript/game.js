3985032a31339bfba8ccc3af403606c95b583982
Jasmine Fitts <jasminefitts@jasmines-mbp.attlocal.net>
2018-05-11 20:17:23 -0400

    updated html, css, and js files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,69 @@
 $(document).ready(function() {
 
+ //The player is shown a random number at the start of the game and it is between 19-120
+    var randomNumber = Math.floor(Math.random() * 101+ 19);
+    console.log(randomNumber);
+
+//Displays random number into the randomNumber id in the html file
+    $("#randomNumber").text(randomNumber);
 
     //Global variables
 var wins = 0;
 var losses = 0; 
-var randomNumber = [""];
-var randomValue = [""];
+var randomNumber;
 var totalScore = [""];
 
-var crystals = ["assets/images/blue.png", "assests/images/green.png", "assets/images/red.pgn", "assets/images/yellow.png"];
+$("#wins").text(wins);
+$("#losses").text(losses);
 
-//The player is shown a random number at the start of the game and it is between 19-120
 
-var randomNumber = Math.floor(Math.random() * 101+ 19);
-    console.log(randomNumber);
-
-$("#randomNumber").text(randomNumber);
 
 
 //Each crystal has a random value between 1-12
 
+var crystals = ["assets/images/blue.png", "assests/images/green.png", "assets/images/red.pgn", "assets/images/yellow.png"];
+
+var redCrystal = Math.floor(Math.random() * 12 + 1)
+var blueCrystal = Math.floor(Math.random() * 12 + 1)
+var greenCrystal = Math.floor(Math.random() * 12 + 1)
+var yellowCrystal = Math.floor(Math.random() * 12 + 1)
+
+totalScore = 0;
+$("#finalScore").text(totalScore)
+
+
+
+$("#redCrystal").on("click", function() {
+    randomNumber = redCrystal + totalScore
+    $("totalScore").text(totalScore)
+    console.log(randomNumber)
+
+    $("#finalScore")
+    
+});
+
+$("#blueCrystal").on("click", function() {
+    randomNumber = totalScore + blueCrystal
+    console.log(randomNumber)
+   
+});
+
+$("#greenCrystal").on("click", function() {
+    randomNumber = totalScore + greenCrystal
+    console.log(randomNumber)
+});
+
+$("#yellowCrystal").on("click", function() {
+    randomNumber = totalScore + yellowCrystal
+    console.log(randomNumber)
+});
+
+
+
    
 
-    $(".crystal").on('click', function () {
-        var randomValue = Math.floor(Math.random() * 12 + 1);
+    $(".redcrystal").on('click', function () {
+        var randomValue = Math.floor(Math.random() * 11 + 1);
         ('data-random');
 
     })
@@ -41,13 +81,32 @@ var randomValue = Math.floor(Math.random() * 12 + 1);
 
 //The player clicks on a crystal and it adds an amount to the player's total score
 
+
+
 //The game hide the amount of the crysal until the player clicks ont he crsytal
 
 //The player wins if their total score matches the random number
 
+function winner() {
+    alert("You Won!!!");
+    wins++
+    $('#wins').text(wins);
+    reset();
+}
+
 //The palyer loses if their score goes above the random number
 
+function loser () {
+    alert("You Lost!");
+    losses++;
+    $('#losses').text(losses);
+    reset()
+}
 
 
 //The game restarts whenever the player wins or loses
+function reset() {
+    randomNumber = Math.floor(Math.random()* 101+ 19);
+
+}
 
