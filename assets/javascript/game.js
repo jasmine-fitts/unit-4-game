3717b6934dc7502375864270fa9cc21b81617534
Jasmine Fitts <jasminefitts@jasmines-mbp.attlocal.net>
2018-05-12 08:09:10 -0400

    last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,10 @@
+
+//Global variables
+var wins = 0;
+var losses = 0; 
+var randomNumber;
+var totalScore = [""];
+
 $(document).ready(function() {
 
  //The player is shown a random number at the start of the game and it is between 19-120
@@ -8,18 +15,11 @@ $(document).ready(function() {
 //Displays random number into the randomNumber id in the html file
     $("#randomNumber").text(randomNumber);
 
-    //Global variables
-var wins = 0;
-var losses = 0; 
-var randomNumber;
-var totalScore = [""];
 
-
-$("#wins").text(wins);
-$("#losses").text(losses);
 
 
 //Each crystal has a random value between 1-12
+
 
 var redCrystal = Math.floor(Math.random() * 12 + 1)
 var blueCrystal = Math.floor(Math.random() * 12 + 1)
@@ -29,18 +29,38 @@ var yellowCrystal = Math.floor(Math.random() * 12 + 1)
 totalScore = 0;
 
 
+$("#wins").text(wins);
+$("#losses").text(losses);
+function reset (){
+    
+};
+
+//The player clicks on a crystal and it adds an amount to the player's total score
+
+
 $("#redCrystal").on("click", function() {
     totalScore = totalScore + redCrystal
     console.log("New total " + totalScore);
     $("#finalTotal").text(totalScore);
 
+    //The game hides the amount of the crysal until the player clicks on the crsytal
+
+
+    //The player wins if their total score matches the random number
     if (totalScore === randomNumber) {
         alert("You Win!");
-        Wins++
+        wins++;
+        $("#wins").text(wins);
+        reset();
     }
+
+
+    //The palyer loses if their score goes above the random number
      else if (totalScore > randomNumber) {
          alert("You Lost :(");
-         losses++
+         losses++;
+         $("#losses").text(losses);
+         reset();
      }   
     
 });
@@ -52,11 +72,15 @@ $("#blueCrystal").on("click", function() {
 
     if (totalScore === randomNumber) {
         alert("You Win!");
-        wins++
+        wins++;
+        $("#wins").text(wins);
+        reset();
     }
      else if (totalScore > randomNumber) {
          alert("You Lost :(");
-         losses++
+         losses++;
+         $("#losses").text(losses);
+         reset();
      }   
    
 });
@@ -68,11 +92,15 @@ $("#greenCrystal").on("click", function() {
 
     if (totalScore === randomNumber) {
         alert("You Win!");
-        wins++
+        wins++;
+        $("#wins").text(wins);
+        reset();
     }
      else if (totalScore > randomNumber) {
          alert("You Lost :(");
-         losses++
+         losses++;
+         $("#losses").text(losses);
+         reset();
      }   
     
 });
@@ -84,46 +112,25 @@ $("#yellowCrystal").on("click", function() {
 
     if (totalScore === randomNumber) {
         alert("You Win!");
-        wins++
+        wins++;
+        $("#wins").text(wins);
+        reset();
     }
      else if(totalScore > randomNumber) {
          alert("You Lost :(");
-         losses++
+         losses++;
+         $("#losses").text(losses);
+         reset();
      }   
-    
+     
 });
 
 
-})
 
-
-
-//The player clicks on a crystal and it adds an amount to the player's total score
-
-
-
-//The game hide the amount of the crysal until the player clicks ont he crsytal
-
-//The player wins if their total score matches the random number
-
-function win() {
-    alert("You won!!");
-    wins
-}
-
-
-//The palyer loses if their score goes above the random number
-
+});
 
 
 //The game restarts whenever the player wins or loses
-function reset() {
-    totalScore = Math.floor(Math.random() * 101 + 19);
-console.log(totalScore);
 
-var redCrystal = Math.floor(Math.random() * 12 + 1)
-var blueCrystal = Math.floor(Math.random() * 12 + 1)
-var greenCrystal = Math.floor(Math.random() * 12 + 1)
-var yellowCrystal = Math.floor(Math.random() * 12 + 1)
-}
+
 
